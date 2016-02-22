iosifpeterfi/schnellnode-pve-ha-manager

commit 9da84a0d51dcc1e1b80e2a92127749de38851e5f

fix 'change_service_location' misuse and recovery from fencing
First rename the change_service_location method from the environment
to an more fitting name, 'steal_service'.

The 'change_service_location' from the virtual hardware class stays
at it is, because there the name fits (those function have not the
same meaning, so it's good that they named different now).

As we misused the config steal method (former
change_service_location) in the stopped state to process the
services from fenced nodes we need another way now.

This is achieved through the private method 'recover_fenced_service'
which is now the only place who has the right to steal a service
from an node.
When a node was successfully fenced we no longer change its services
state to 'stopped', rather we drop that hack and search a new node
in 'recover_fenced_service', if found we the steal the service and
move it to from the fenced to the new (recovery) node and place it
there in the 'started' state, after that the state machine is able
to handle the rest.

If we do not find a node we try again next round as that is better
then placing it in the error state, because so we have still a
chance to recover, which we do not have with the error state.

Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

src/PVE/HA/Manager.pm
@@ -213,6 +213,44 @@ my $change_service_state = sub {
     $haenv->log('info', "service '$sid': state changed from '${old_state}' to '${new_state}' $text_state");
 };
 
+# after a node was fenced this recovers the service to a new node
+my $recover_fenced_service = sub {
+    my ($self, $sid, $cd) = @_;
+
+    my ($haenv, $ss) = ($self->{haenv}, $self->{ss});
+
+    my $sd = $ss->{$sid};
+
+    if ($sd->{state} ne 'fence') { # should not happen
+	$haenv->log('err', "cannot recover service '$sid' from fencing," .
+		    " wrong state '$sd->{state}'");
+	return;
+    }
+
+    my $fenced_node = $sd->{node}; # for logging purpose
+
+    $self->recompute_online_node_usage(); # we want the most current node state
+
+    my $recovery_node = select_service_node($self->{groups},
+					    $self->{online_node_usage},
+					    $cd, $sd->{node});
+
+    if ($recovery_node) {
+	$haenv->log('info', "recover service '$sid' from fenced node " .
+		    "'$fenced_node' to node '$recovery_node'");
+
+	$haenv->steal_service($sid, $sd->{node}, $recovery_node);
+
+	# $sd *is normally read-only*, fencing is the exception
+	$cd->{node} = $sd->{node} = $recovery_node;
+	&$change_service_state($self, $sid, 'started', node => $recovery_node);
+    } else {
+	# no node found, let the service in 'fence' state and try again
+	$haenv->log('err', "recovering service '$sid' from fenced node " .
+		    "'$fenced_node' failed, no recovery node found");
+    }
+};
+
 # read LRM status for all nodes 
 sub read_lrm_status {
     my ($self) = @_;
@@ -380,8 +418,8 @@ sub manage {
 
 	    next if !$fenced_nodes->{$sd->{node}};
 
-	    # node fence was successful - mark service as stopped
-	    &$change_service_state($self, $sid, 'stopped');	    
+	    # node fence was successful - recover service
+	    &$recover_fenced_service($self, $sid, $sc->{$sid});
 	}
 
 	last if !$repeat;
@@ -471,14 +509,8 @@ sub next_state_stopped {
 	    } elsif ($sd->{node} eq $target) {
 		$haenv->log('info', "ignore service '$sid' $cmd request - service already on node '$target'");
 	    } else {
-		eval {
-		    $haenv->change_service_location($sid, $sd->{node}, $target);
-		    $cd->{node} = $sd->{node} = $target; # fixme: $sd is read-only??!!	    
-		    $haenv->log('info', "$cmd service '$sid' to node '$target' (stopped)");
-		};
-		if (my $err = $@) {
-		    $haenv->log('err', "$cmd service '$sid' to node '$target' failed - $err");
-		}
+		&$change_service_state($self, $sid, $cmd, node => $target);
+		return;
 	    }
 	} else {
 	    $haenv->log('err', "unknown command '$cmd' for service '$sid'"); 
@@ -491,24 +523,9 @@ sub next_state_stopped {
     } 
 
     if ($cd->{state} eq 'enabled') {
-	if (my $node = select_service_node($self->{groups}, $self->{online_node_usage}, $cd, $sd->{node})) {
-	    if ($node && ($sd->{node} ne $node)) {
-		eval {
-		    $haenv->change_service_location($sid, $sd->{node}, $node);
-		    $cd->{node} = $sd->{node} = $node; # fixme: $sd is read-only??!!
-		};
-		if (my $err = $@) {
-		    $haenv->log('err', "move service '$sid' to node '$node' failed - $err");
-		} else {
-		    &$change_service_state($self, $sid, 'started', node => $node);
-		}
-	    } else {
-		&$change_service_state($self, $sid, 'started', node => $node);
-	    }
-	} else {
-	    # fixme: warn 
-	}
-
+	# simply mark it started, if it's on the wrong node
+	# next_state_started will fix that for us
+	&$change_service_state($self, $sid, 'started', node => $sd->{node});
 	return;
     }
 

src/test/test-basic1/log.expect
@@ -43,8 +43,8 @@ info    166     hardware: server 'node3' stopped by poweroff (watchdog)
 info    240    node1/crm: got lock 'ha_agent_node3_lock'
 info    240    node1/crm: fencing: acknowleged - got agent lock for node 'node3'
 info    240    node1/crm: node 'node3': state changed from 'fence' => 'unknown'
-info    240    node1/crm: service 'vm:103': state changed from 'fence' to 'stopped' 
-info    260    node1/crm: service 'vm:103': state changed from 'stopped' to 'started'  (node = node2)
-info    263    node2/lrm: starting service vm:103
-info    263    node2/lrm: service status vm:103 started
+info    240    node1/crm: recover service 'vm:103' from fenced node 'node3' to node 'node2'
+info    240    node1/crm: service 'vm:103': state changed from 'fence' to 'started'  (node = node2)
+info    243    node2/lrm: starting service vm:103
+info    243    node2/lrm: service status vm:103 started
 info    720     hardware: exit simulation - done

src/test/test-basic2/log.expect
@@ -11,12 +11,12 @@ info     22    node3/crm: node 'node2': state changed from 'online' => 'unknown'
 info     22    node3/crm: got lock 'ha_agent_node1_lock'
 info     22    node3/crm: fencing: acknowleged - got agent lock for node 'node1'
 info     22    node3/crm: node 'node1': state changed from 'fence' => 'unknown'
-info     22    node3/crm: service 'vm:101': state changed from 'fence' to 'stopped' 
+info     22    node3/crm: recover service 'vm:101' from fenced node 'node1' to node 'node3'
+info     22    node3/crm: service 'vm:101': state changed from 'fence' to 'started'  (node = node3)
+info     23    node3/lrm: got lock 'ha_agent_node3_lock'
+info     23    node3/lrm: status change wait_for_agent_lock => active
+info     23    node3/lrm: starting service vm:101
+info     23    node3/lrm: service status vm:101 started
 info     40    node1/crm: status change wait_for_quorum => slave
 info     42    node3/crm: node 'node1': state changed from 'unknown' => 'online'
-info     42    node3/crm: service 'vm:101': state changed from 'stopped' to 'started'  (node = node1)
-info    161    node1/lrm: got lock 'ha_agent_node1_lock'
-info    161    node1/lrm: status change wait_for_agent_lock => active
-info    161    node1/lrm: starting service vm:101
-info    161    node1/lrm: service status vm:101 started
 info    620     hardware: exit simulation - done

src/test/test-basic5/log.expect
@@ -46,8 +46,8 @@ info    282    node3/crm: node 'node1': state changed from 'unknown' => 'fence'
 info    282    node3/crm: got lock 'ha_agent_node1_lock'
 info    282    node3/crm: fencing: acknowleged - got agent lock for node 'node1'
 info    282    node3/crm: node 'node1': state changed from 'fence' => 'unknown'
-info    282    node3/crm: service 'vm:101': state changed from 'fence' to 'stopped' 
-info    282    node3/crm: service 'vm:101': state changed from 'stopped' to 'started'  (node = node2)
+info    282    node3/crm: recover service 'vm:101' from fenced node 'node1' to node 'node2'
+info    282    node3/crm: service 'vm:101': state changed from 'fence' to 'started'  (node = node2)
 info    301    node2/lrm: starting service vm:101
 info    301    node2/lrm: service status vm:101 started
 info    500      cmdlist: execute power node1 on

src/test/test-shutdown1/log.expect
@@ -34,8 +34,8 @@ info    200    node1/crm: node 'node3': state changed from 'unknown' => 'fence'
 info    200    node1/crm: got lock 'ha_agent_node3_lock'
 info    200    node1/crm: fencing: acknowleged - got agent lock for node 'node3'
 info    200    node1/crm: node 'node3': state changed from 'fence' => 'unknown'
-info    200    node1/crm: service 'vm:103': state changed from 'fence' to 'stopped' 
-info    200    node1/crm: service 'vm:103': state changed from 'stopped' to 'started'  (node = node1)
+info    200    node1/crm: recover service 'vm:103' from fenced node 'node3' to node 'node1'
+info    200    node1/crm: service 'vm:103': state changed from 'fence' to 'started'  (node = node1)
 info    201    node1/lrm: got lock 'ha_agent_node1_lock'
 info    201    node1/lrm: status change wait_for_agent_lock => active
 info    201    node1/lrm: starting service vm:103

src/test/test-shutdown2/log.expect
@@ -34,8 +34,8 @@ info    200    node1/crm: node 'node3': state changed from 'unknown' => 'fence'
 info    200    node1/crm: got lock 'ha_agent_node3_lock'
 info    200    node1/crm: fencing: acknowleged - got agent lock for node 'node3'
 info    200    node1/crm: node 'node3': state changed from 'fence' => 'unknown'
-info    200    node1/crm: service 'vm:103': state changed from 'fence' to 'stopped' 
-info    200    node1/crm: service 'vm:103': state changed from 'stopped' to 'started'  (node = node1)
+info    200    node1/crm: recover service 'vm:103' from fenced node 'node3' to node 'node1'
+info    200    node1/crm: service 'vm:103': state changed from 'fence' to 'started'  (node = node1)
 info    201    node1/lrm: got lock 'ha_agent_node1_lock'
 info    201    node1/lrm: status change wait_for_agent_lock => active
 info    201    node1/lrm: starting service vm:103

src/test/test-shutdown3/log.expect
@@ -34,8 +34,8 @@ info    200    node1/crm: node 'node3': state changed from 'unknown' => 'fence'
 info    200    node1/crm: got lock 'ha_agent_node3_lock'
 info    200    node1/crm: fencing: acknowleged - got agent lock for node 'node3'
 info    200    node1/crm: node 'node3': state changed from 'fence' => 'unknown'
-info    200    node1/crm: service 'ct:103': state changed from 'fence' to 'stopped' 
-info    200    node1/crm: service 'ct:103': state changed from 'stopped' to 'started'  (node = node1)
+info    200    node1/crm: recover service 'ct:103' from fenced node 'node3' to node 'node1'
+info    200    node1/crm: service 'ct:103': state changed from 'fence' to 'started'  (node = node1)
 info    201    node1/lrm: got lock 'ha_agent_node1_lock'
 info    201    node1/lrm: status change wait_for_agent_lock => active
 info    201    node1/lrm: starting service ct:103

src/test/test-shutdown4/log.expect
@@ -37,8 +37,8 @@ info    220    node2/crm: node 'node1': state changed from 'unknown' => 'fence'
 info    220    node2/crm: got lock 'ha_agent_node1_lock'
 info    220    node2/crm: fencing: acknowleged - got agent lock for node 'node1'
 info    220    node2/crm: node 'node1': state changed from 'fence' => 'unknown'
-info    220    node2/crm: service 'vm:100': state changed from 'fence' to 'stopped' 
-info    220    node2/crm: service 'vm:100': state changed from 'stopped' to 'started'  (node = node2)
+info    220    node2/crm: recover service 'vm:100' from fenced node 'node1' to node 'node2'
+info    220    node2/crm: service 'vm:100': state changed from 'fence' to 'started'  (node = node2)
 info    221    node2/lrm: got lock 'ha_agent_node2_lock'
 info    221    node2/lrm: status change wait_for_agent_lock => active
 info    221    node2/lrm: starting service vm:100

commit 9dad9c8823600f0192038e5779184cd21134e79f

fix 'uninitialized value' on online node usage computation
This fixes a bug introduced by commit 9da84a0 which set the wrong
hash when a disabled service got a migrate/relocate command.

We set "node => $target", while our state machine could handle that
we got some "uninitialized value" warnings when migrating a disabled
service to an inactive LRM. Better set "target => $target"

Further add a test for this scenario.

Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

src/PVE/HA/Manager.pm
@@ -509,7 +509,8 @@ sub next_state_stopped {
 	    } elsif ($sd->{node} eq $target) {
 		$haenv->log('info', "ignore service '$sid' $cmd request - service already on node '$target'");
 	    } else {
-		&$change_service_state($self, $sid, $cmd, node => $target);
+		&$change_service_state($self, $sid, $cmd, node => $sd->{node},
+				       target => $target);
 		return;
 	    }
 	} else {

src/test/test-relocate-to-inactive-node/README
@@ -1,0 +1,1 @@
+Test user triggered service migration.

src/test/test-relocate-to-inactive-node/cmdlist
@@ -1,0 +1,4 @@
+[
+    [ "power node1 on", "power node2 on", "power node3 on"],
+    [ "service vm:103 relocate node2" ]
+]

src/test/test-relocate-to-inactive-node/hardware_status
@@ -1,0 +1,5 @@
+{
+  "node1": { "power": "off", "network": "off" },
+  "node2": { "power": "off", "network": "off" },
+  "node3": { "power": "off", "network": "off" }
+}

src/test/test-relocate-to-inactive-node/log.expect
@@ -1,0 +1,33 @@
+info      0     hardware: starting simulation
+info     20      cmdlist: execute power node1 on
+info     20    node1/crm: status change startup => wait_for_quorum
+info     20    node1/lrm: status change startup => wait_for_agent_lock
+info     20      cmdlist: execute power node2 on
+info     20    node2/crm: status change startup => wait_for_quorum
+info     20    node2/lrm: status change startup => wait_for_agent_lock
+info     20      cmdlist: execute power node3 on
+info     20    node3/crm: status change startup => wait_for_quorum
+info     20    node3/lrm: status change startup => wait_for_agent_lock
+info     20    node1/crm: got lock 'ha_manager_lock'
+info     20    node1/crm: status change wait_for_quorum => master
+info     20    node1/crm: node 'node1': state changed from 'unknown' => 'online'
+info     20    node1/crm: node 'node2': state changed from 'unknown' => 'online'
+info     20    node1/crm: node 'node3': state changed from 'unknown' => 'online'
+info     20    node1/crm: adding new service 'vm:103' on node 'node3'
+info     20    node1/crm: service 'vm:103': state changed from 'started' to 'request_stop'
+info     22    node2/crm: status change wait_for_quorum => slave
+info     24    node3/crm: status change wait_for_quorum => slave
+info     25    node3/lrm: got lock 'ha_agent_node3_lock'
+info     25    node3/lrm: status change wait_for_agent_lock => active
+info     40    node1/crm: service 'vm:103': state changed from 'request_stop' to 'stopped'
+info    120      cmdlist: execute service vm:103 relocate node2
+info    120    node1/crm: got crm command: relocate vm:103 node2
+info    120    node1/crm: service 'vm:103': state changed from 'stopped' to 'relocate'  (node = node3, target = node2)
+info    125    node3/lrm: service vm:103 - start relocate to node 'node2'
+info    125    node3/lrm: service vm:103 - end relocate to node 'node2'
+info    140    node1/crm: service 'vm:103': state changed from 'relocate' to 'started'  (node = node2)
+info    140    node1/crm: service 'vm:103': state changed from 'started' to 'request_stop'
+info    143    node2/lrm: got lock 'ha_agent_node2_lock'
+info    143    node2/lrm: status change wait_for_agent_lock => active
+info    160    node1/crm: service 'vm:103': state changed from 'request_stop' to 'stopped'
+info    720     hardware: exit simulation - done

src/test/test-relocate-to-inactive-node/manager_status
@@ -1,0 +1,1 @@
+{}

src/test/test-relocate-to-inactive-node/service_config
@@ -1,0 +1,3 @@
+{
+    "vm:103": { "node": "node3", "state": "disabled" }
+}